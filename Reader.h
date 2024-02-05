d6e5d1b7858eade07ce2ccac894f401e5844d8f6
Your Name <you@example.com>
2024-02-05 16:36:58 -0500

    TESTING  FOR AMEND Testing commits from Visual Studio

--- Reader.h
+++ Reader.h
@@ -5,6 +5,9 @@
 * Author: Hamza El Sousi && Mohammed Alashi
 * Professors: Paulo Sousa
 * Student Names: Hamza El Sousi && Mohammad Alashi
+* Student ID: 040982818 && 01470990
+* 
+* TEST 
 ************************************************************
 =---------------------------------------------=
 |      COMPILERS - ALGONQUIN COLLEGE (W24)    |
