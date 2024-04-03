9433b740faa4e1a3be3fe5c7c6064a3bef16b365
Hamza El Sousi <elso0011@algonquinlive.com>
2024-04-03 15:50:00 -0400

    included Parser files

---
+++ Parser.h
@@ -0,0 +1,152 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: Winter, 2024
+* Author: Hamza El Sousi && Mohammad Alashi
+* Professors: Paulo Sousa
+* Student Names: Hamza El Sousi && Mohammad Alashi
+* Assignment A32 Due Date: April 14 2024
+************************************************************
+=---------------------------------------------=
+|      COMPILERS - ALGONQUIN COLLEGE (W24)    |
+=---------------------------------------------=
+
+	 d8888   .d8888b.                    888
+	d8P888  d88P  Y88b                   888
+   d8P 888  888    888                   888
+  d8P  888  888         8888b.  .d8888b  888888
+ d88   888  888            "88b 88K      888
+ 8888888888 888    888 .d888888 "Y8888b. 888
+	   888  Y88b  d88P 888  888      X88 Y88b.
+	   888   "Y8888P"  "Y888888  88888P'  "Y888
+ */
+
+
+/*
+************************************************************
+* File name: Parser.h
+* Compiler: MS Visual Studio 2022
+* Course: CST 8152 – Compilers, Lab Section: [011, 012]
+* Assignment: A32.
+* Date: May 01 2023
+* Professor: Paulo Sousa
+* Purpose: This file is the main header for Parser (.h)
+************************************************************
+*/
+
+#ifndef PARSER_H_
+#define PARSER_H_
+
+/* Inclusion section */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#ifndef COMPILERS_H_
+#include "Compilers.h"
+#endif
+#ifndef READER_H_
+#include "Reader.h"
+#endif
+#ifndef SCANNER_H_
+#include "Scanner.h"
+#endif
+
+/* Global vars */
+static Token			lookahead;
+extern BufferPointer	stringLiteralTable;
+extern int		line;
+extern Token			tokenizer(Cast_void);
+extern str		keywordTable[KWT_SIZE];
+static int		syntaxErrorNumber = 0;
+
+#define LANG_WRTE		"print&"
+#define LANG_READ		"input&"
+#define LANG_MAIN		"main&"
+
+/* TO_DO: Create ALL constants for keywords (sequence given in table.h) */
+
+/* Constants */
+enum KEYWORDS {
+	NO_ATTR = -1,
+	KW_data,
+	KW_code,
+	KW_int,
+	KW_real,
+	KW_string,
+	KW_if,
+	KW_then,
+	KW_else,
+	KW_while,
+	KW_do
+};
+
+/* TO_DO: Define the number of BNF rules */
+#define NUM_BNF_RULES 12
+
+/* Parser */
+typedef struct parserData {
+	int parsHistogram[NUM_BNF_RULES];	/* Number of BNF Statements */
+} ParserData, * pParsData;
+
+/* Number of errors */
+int numParserErrors;
+
+/* Scanner data */
+ParserData psData;
+
+/* Function definitions */
+Cast_void startParser();
+Cast_void matchToken(int, int);
+Cast_void syncErrorHandler(int);
+Cast_void printError();
+Cast_void printBNFData(ParserData psData);
+
+/* List of BNF statements */
+enum BNF_RULES {
+	BNF_error,										/*  0: Error token */
+	BNF_codeSession,								/*  1 */
+	BNF_comment,									/*  2 */
+	BNF_dataSession,								/*  3 */
+	BNF_optVarListDeclarations,						/*  4 */
+	BNF_optionalStatements,							/*  5 */
+	BNF_outputStatement,							/*  6 */
+	BNF_outputVariableList,							/*  7 */
+	BNF_program,									/*  8 */
+	BNF_statement,									/*  9 */
+	BNF_statements,									/* 10 */
+	BNF_statementsPrime								/* 11 */
+};
+
+
+/* TO_DO: Define the list of keywords */
+static str BNFStrTable[NUM_BNF_RULES] = {
+	"BNF_error",
+	"BNF_codeSession",
+	"BNF_comment",
+	"BNF_dataSession",
+	"BNF_optVarListDeclarations",
+	"BNF_optionalStatements",
+	"BNF_outputStatement",
+	"BNF_outputVariableList",
+	"BNF_program",
+	"BNF_statement",
+	"BNF_statements",
+	"BNF_statementsPrime"
+};
+
+/* TO_DO: Place ALL non-terminal function declarations */
+Cast_void codeSession();
+Cast_void comment();
+Cast_void dataSession();
+Cast_void optVarListDeclarations();
+Cast_void optionalStatements();
+Cast_void outputStatement();
+Cast_void outputVariableList();
+Cast_void program();
+Cast_void statement();
+Cast_void statements();
+Cast_void statementsPrime();
+
+#endif
